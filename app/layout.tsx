30eb20b81e9e9f9f590e4a7d057ffdcae8982e77
pshycodr <115141578+pshycodr@users.noreply.github.com>
2025-09-24 13:26:28 +0530

    add: page view tracker to trigger page url event

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,12 @@ import Footer from "@/components/common/Footer";
 import Header from "@/components/common/Navbar";
 import ContactUs from "@/components/sections/ContactUs";
 import { GoogleTagManager } from "@/components/ui/GoogleTagManager";
+import { PageViewTracker } from "@/components/ui/PageViewTracker";
 import StickyContactButtons from "@/components/ui/StickyButtons";
 import type { Metadata } from "next";
 import { playfairDisplay, poppins } from "./fonts";
 import "./globals.css";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "MifTech Solution",
@@ -23,7 +25,10 @@ export default function RootLayout({
       <body
         className={`${poppins.variable} ${playfairDisplay.variable} font-poppins`}
       >
-       
+        <Suspense>
+          <PageViewTracker />
+        </Suspense>
+        
         <GoogleTagManager />
 
         <Header />
