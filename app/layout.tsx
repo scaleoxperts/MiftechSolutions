2f2697f30a130ccdf304ef72ced978c8943eb0fe
pshycodr <115141578+pshycodr@users.noreply.github.com>
2025-09-29 13:19:03 +0530

    add: gtm from @next/third-parties package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import Footer from "@/components/common/Footer";
 import Header from "@/components/common/Navbar";
 import ContactUs from "@/components/sections/ContactUs";
-import { GoogleTagManager } from "@/components/ui/GoogleTagManager";
 import { PageViewTracker } from "@/components/ui/PageViewTracker";
 import StickyContactButtons from "@/components/ui/StickyButtons";
+import { GoogleTagManager } from "@next/third-parties/google";
 import type { Metadata } from "next";
+import { Suspense } from "react";
 import { playfairDisplay, poppins } from "./fonts";
 import "./globals.css";
-import { Suspense } from "react";
-import Script from "next/script";
+import { GTMTracker } from "@/components/ui/GTMTracker";
+
 
 export const metadata: Metadata = {
   title: "MifTech Solution",
@@ -23,35 +24,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        {/* Google Tag Manager */}
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WSBLTDWJ');
-          `}
-        </Script>
-      </head>
+      <GoogleTagManager gtmId={"GTM-WSBLTDWJ"} />
       <body
         className={`${poppins.variable} ${playfairDisplay.variable} font-poppins`}
       >
 
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=GTM-WSBLTDWJ`}
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
-
-        <GoogleTagManager />
-
         <Suspense>
           <PageViewTracker />
+          <GTMTracker />
         </Suspense>
 
         <Header />
